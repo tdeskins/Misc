96d1fff023fa4d4f766202a1cc7f86c03877641c
Thomas Deskins <13tdeskins@gmail.com>
2018-02-25 13:07:43 -0500

    debugged, used Euclid's algorithm to find LCM of i and j

--- e1improved.java
+++ e1improved.java
@@ -3,6 +3,12 @@ import java.util.*;
 
 public class e1improved
 {
+	// Use Euclid's algorithm to find gcd
+	public static int gcd(int p, int q)
+	{
+		if (q ==0) return p;
+		else return gcd(q, p % q);
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("Sum of multiples of i and j up to, but not including, end value");
@@ -19,34 +25,40 @@ public class e1improved
 		System.out.print("end: ");
 		int end = sc.nextInt();
 
-		// GCF
-		double iGCF = Math.floor(end/i)*i;
-		if(iGCF==end)
+		// Define Largest Multiple of i and j less than end
+		double iLM = Math.floor(end/i)*i;
+		if(iLM==end)
 		{
-			iGCF = end - i;
+			iLM = end - i;
 		}
-		double jGCF = Math.floor(end/j)*j;
-		if(jGCF==end)
+		double jLM = Math.floor(end/j)*j;
+		if(jLM==end)
 		{
-			jGCF = end - j;
+			jLM = end - j;
 		}
-		double ijGCF = Math.floor(end/(i*j))*i*j;
+		
+		// Euclid's algorithm is used, product/gcd is the least common multiple
+		int d = gcd(i,j);
+		// ij is the LCM of i and j
+		int ij = (i*j)/d;
+		
+		// Find the Largest Multiple of the LCM of i and j less than end
+		double ijGCF = Math.floor(end/(ij))*ij;
 		if(ijGCF==end)
 		{
-			ijGCF = end - i*j;
+			ijGCF = end - ij;
 		}
+		System.out.println("Largest Common Multiple of i and j: " + ij);
+		System.out.println("Largest Multiple of i < end: " + (int) iLM);
+		System.out.println("Largest Mutliple of j < end: " + (int) jLM);
+		System.out.println("Largest Multiple of LCM i,j < end: " + (int) ijGCF);
 
-		System.out.println("iGCF: " + (int) iGCF);
-		System.out.println("jGCF: " + (int) jGCF);
-		System.out.println("ijGCF: " + (int) ijGCF);
-
-		double it = (0.5/i)*(i+iGCF)*(iGCF);
-		double jt = (0.5/j)*(j+jGCF)*(jGCF);
-		double ijt = (0.5/(i*j))*((i*j)+ijGCF)*(ijGCF);
+		double it = (0.5/i)*(i+iLM)*(iLM);
+		double jt = (0.5/j)*(j+jLM)*(jLM);
+		double ijt = (0.5/(ij))*((ij)+ijGCF)*(ijGCF);
 		double sum = it + jt - ijt;
 		int intsum = (int) sum;
 		System.out.println("Sum is " + intsum);
-
 	}
 }
 		
