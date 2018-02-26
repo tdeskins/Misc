7326a2e1fcbca7d443755ab3cde6c698095e7aea
Thomas Deskins <13tdeskins@gmail.com>
2018-02-26 01:06:24 -0500

    Codeforces 339A - Helpful Maths - greedy algorithm, implementation, sortings, strings

---
+++ e3_LargestPrimeFactor.java
@@ -0,0 +1,12 @@
+public class e3_LargestPrimeFactor
+{
+	static int SoE(int n)
+	{
+		for  
+	}
+	public static void main(String[] args)
+	{
+		//What is the largest prime factor of the number 600851475143
+		
+	}
+}
